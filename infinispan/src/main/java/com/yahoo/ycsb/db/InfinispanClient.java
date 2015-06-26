ebf08051e65f71872583983f7c2343fd13ef0578
otrack <0track@gmail.com>
2015-06-26 17:07:05 +0200

    [FIX] Ensemble support

--- infinispan/src/main/java/com/yahoo/ycsb/db/InfinispanClient.java
+++ infinispan/src/main/java/com/yahoo/ycsb/db/InfinispanClient.java
@@ -7,6 +7,7 @@ import com.yahoo.ycsb.StringByteIterator;
 import org.infinispan.commons.api.BasicCache;
 import org.infinispan.commons.api.BasicCacheContainer;
 import org.infinispan.ensemble.EnsembleCacheManager;
+import org.infinispan.ensemble.indexing.LocalIndexBuilder;
 import org.infinispan.util.logging.Log;
 import org.infinispan.util.logging.LogFactory;
 
@@ -23,106 +24,121 @@ import java.util.*;
  */
 public class InfinispanClient extends DB {
 
-    private static final int OK = 0;
-    private static final int ERROR = -1;
-    private static final int NOT_FOUND = -2;
+   private static final int OK = 0;
+   private static final int ERROR = -1;
+   private static final int NOT_FOUND = -2;
 
-    private boolean _debug = false;
+   private boolean _debug = false;
 
-    private BasicCacheContainer infinispanManager;
-    private BasicCache<String, Map<String, String>> cache;
+   private static final Log logger = LogFactory.getLog(InfinispanClient.class);
+   private static BasicCacheContainer infinispanManager;
+   private static BasicCache<String, Map<String, String>> cache;
 
-    private static final Log logger = LogFactory.getLog(InfinispanClient.class);
+   public InfinispanClient() {
+   }
 
-    public InfinispanClient() {
-    }
+   public synchronized void init() throws DBException {
+      
+      synchronized (InfinispanClient.class) {
 
-    public void init() throws DBException {
+         if (cache!=null)
+            return;
+         
+         String host = getProperties().getProperty("host");
+         _debug = Boolean.parseBoolean(getProperties().getProperty("debug", "false"));
 
-        String host = getProperties().getProperty("host");
-        _debug = Boolean.parseBoolean(getProperties().getProperty("debug", "false"));
-
-        if (host == null)
+         if (host == null)
             throw new RuntimeException("Required property \"host\" missing for InfinispanClient");
 
-        try {
-           infinispanManager = new EnsembleCacheManager(Arrays.asList(host.split(",")));
+         try {
+            Properties properties = new Properties();
+            properties.setProperty("infinispan.client.hotrod.default_executor_factory.pool_size", "100");
+
+            infinispanManager = new EnsembleCacheManager(
+                  host,
+                  null,
+                  properties,
+                  new LocalIndexBuilder());
             cache = infinispanManager.getCache();
-        } catch (Exception e) {
+
+         } catch (Exception e) {
             e.printStackTrace();
-        }
+         }
 
-    }
+      }
 
-    public void cleanup() {
-        infinispanManager.stop();
-        infinispanManager = null;
-    }
 
-    public int read(String table, String key, Set<String> fields, HashMap<String, ByteIterator> result) {
-        try {
-            Map<String, String> row;
-            row = cache.get(key);
-            if (row != null) {
-                result.clear();
-                if (fields == null || fields.isEmpty()) {
-                    StringByteIterator.putAllAsByteIterators(result, row);
-                } else {
-                    for (String field : fields){
-                        result.put(field, new StringByteIterator(row.get(field)));
-                    }
-                }
-            }
-            if (_debug)
-                System.out.println("Reading key: " + key+"("+result+")");
-            return OK;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ERROR;
-        }
-    }
+   }
 
-    public int scan(String table, String startkey, int recordcount, Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
-        logger.warn("Infinispan does not support scan semantics");
-        return OK;
-    }
+   public void cleanup() {
+      infinispanManager.stop();
+      infinispanManager = null;
+   }
 
-    public int update(String table, String key, HashMap<String, ByteIterator> values) {
-        try {
-            Map<String, String> row = cache.get(key);
-            if (row == null) {
-                row = StringByteIterator.getStringMap(values);
-                cache.put(key, row);
+   public int read(String table, String key, Set<String> fields, HashMap<String, ByteIterator> result) {
+      try {
+         Map<String, String> row;
+         row = cache.get(key);
+         if (row != null) {
+            result.clear();
+            if (fields == null || fields.isEmpty()) {
+               StringByteIterator.putAllAsByteIterators(result, row);
             } else {
-                StringByteIterator.putAllAsStrings(row, values);
+               for (String field : fields){
+                  result.put(field, new StringByteIterator(row.get(field)));
+               }
             }
+         }
+         if (_debug)
+            System.out.println("Reading key: " + key+"("+result+")");
+         return OK;
+      } catch (Exception e) {
+         e.printStackTrace();
+         return ERROR;
+      }
+   }
 
-            return OK;
-        } catch (Exception e) {
-            return ERROR;
-        }
-    }
+   public int scan(String table, String startkey, int recordcount, Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
+      logger.warn("Infinispan does not support scan semantics");
+      return OK;
+   }
 
-    public int insert(String table, String key, HashMap<String, ByteIterator> values) {
-        try {
-            Map<String, String> row = new HashMap<String, String>();
+   public int update(String table, String key, HashMap<String, ByteIterator> values) {
+      try {
+         Map<String, String> row = cache.get(key);
+         if (row == null) {
             row = StringByteIterator.getStringMap(values);
-            if (_debug)
-                System.out.println("Inserting key: " + key+"("+row+")");
-            cache.put(key,row);
-            return OK;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ERROR;
-        }
-    }
+            cache.put(key, row);
+         } else {
+            StringByteIterator.putAllAsStrings(row, values);
+         }
 
-    public int delete(String table, String key) {
-        try {
-            cache.remove(key);
-            return OK;
-        } catch (Exception e) {
-            return ERROR;
-        }
-    }
+         return OK;
+      } catch (Exception e) {
+         return ERROR;
+      }
+   }
+
+   public int insert(String table, String key, HashMap<String, ByteIterator> values) {
+      try {
+         Map<String, String> row = new HashMap<String, String>();
+         row = StringByteIterator.getStringMap(values);
+         if (_debug)
+            System.out.println("Inserting key: " + key+"("+row+")");
+         cache.put(key,row);
+         return OK;
+      } catch (Exception e) {
+         e.printStackTrace();
+         return ERROR;
+      }
+   }
+
+   public int delete(String table, String key) {
+      try {
+         cache.remove(key);
+         return OK;
+      } catch (Exception e) {
+         return ERROR;
+      }
+   }
 }
