fd3be30d45766dacca260b7047d9bd13080d19ac
otrack <0track@gmail.com>
2017-10-16 12:49:01 +0200

    fix bindings

--- epaxos/src/main/java/com/yahoo/ycsb/db/EPaxosClient.java
+++ epaxos/src/main/java/com/yahoo/ycsb/db/EPaxosClient.java
@@ -21,19 +21,25 @@ import com.yahoo.ycsb.ByteIterator;
 import com.yahoo.ycsb.DB;
 import com.yahoo.ycsb.DBException;
 import com.yahoo.ycsb.Status;
+import com.yahoo.ycsb.StringByteIterator;
 import go.bindings.Bindings;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
 /**
- * This is a client implementation for Infinispan 5.x.
+ * This is a client implementation for EPaxos.
  */
 public class EPaxosClient extends DB {
 
-  private Bindings.Parameters parameters = Bindings.NewParameters();
+  private Bindings.Parameters epaxos = Bindings.NewParameters();
   private boolean verbose = false;
 
   public EPaxosClient() {
@@ -57,9 +63,9 @@ public class EPaxosClient extends DB {
     }
 
     if (!getProperties().containsKey("host") | !getProperties().containsKey("port")) {
-      parameters.Connect("localhost", 7087, leaderless, fast);
+      epaxos.Connect("localhost", 7087, leaderless, fast);
     } else {
-      parameters.Connect(
+      epaxos.Connect(
           getProperties().getProperty("host"),
           Integer.parseInt(getProperties().getProperty("port")),
           leaderless,
@@ -73,45 +79,88 @@ public class EPaxosClient extends DB {
   }
 
   @Override
-  public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
-    parameters.Read(key.hashCode());
-    if (verbose){
-      System.out.println("READ: "+result.toString());
+  public Status insert(String table, String key, Map<String, ByteIterator> values) {
+    try {
+      byte[] data = marshal(StringByteIterator.getStringMap(values));
+      epaxos.Write(hash(key), data);
+      if (verbose) {
+        System.out.println("UPDATE: " + key + " -> " + values);
+      }
+      return Status.OK;
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    return Status.OK;
+    return Status.ERROR;
+  }
+
+  @Override
+  public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
+    try {
+      result = new HashMap<>();
+      byte[] data = epaxos.Read(hash(key));
+      StringByteIterator.putAllAsByteIterators(result, unmarshal(data));
+      if (verbose) {
+        System.out.println("READ: " + key + " -> " + result);
+      }
+      return Status.OK;
+    } catch (IOException | ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+    return Status.ERROR;
   }
 
   @Override
   public Status scan(String table, String startkey, int recordcount, Set<String> fields,
                      Vector<HashMap<String, ByteIterator>> result) {
-    if (verbose){
-      System.out.println("SCAN: "+result.toString());
+    try {
+      result = new Vector<>();
+      HashMap<String, ByteIterator> item = new HashMap<>();
+      byte[] data = epaxos.Scan(hash(startkey));
+      StringByteIterator.putAllAsByteIterators(item, unmarshal(data));
+      result.add(item);
+      if (verbose) {
+        System.out.println("SCAN: " + startkey + "[0-" + recordcount + "] -> " + result.toString());
+      }
+      return Status.OK;
+    } catch (IOException | ClassNotFoundException e) {
+      e.printStackTrace();
     }
     return Status.ERROR;
   }
 
   @Override
   public Status update(String table, String key, Map<String, ByteIterator> values) {
-    parameters.Write(key.hashCode(), values.toString());
-    if (verbose){
-      System.out.println("UPDATE: "+values.toString());
-    }
-    return Status.OK;
+    return Status.NOT_IMPLEMENTED;
   }
 
-  @Override
-  public Status insert(String table, String key, Map<String, ByteIterator> values) {
-    parameters.Write(key.hashCode(), values.toString());
-    if (verbose){
-      System.out.println("INSERT: "+values.toString());
-    }
-    return Status.OK;
-  }
 
   @Override
   public Status delete(String table, String key) {
-    parameters.Write(key.hashCode(), "");
-    return Status.OK;
+    return Status.NOT_IMPLEMENTED;
+  }
+
+  // adapted from String.hashCode()
+  public static long hash(String string) {
+    long h = 1125899906842597L; // prime
+    int len = string.length();
+
+    for (int i = 0; i < len; i++) {
+      h = 31 * h + string.charAt(i);
+    }
+    return h;
+  }
+
+  private static byte[] marshal(Map<String, String> map) throws IOException {
+    ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+    ObjectOutputStream out = new ObjectOutputStream(byteOut);
+    out.writeObject(map);
+    return byteOut.toByteArray();
+  }
+
+  private static Map<String, String> unmarshal(byte[] data) throws IOException, ClassNotFoundException {
+    ByteArrayInputStream byteIn = new ByteArrayInputStream(data);
+    ObjectInputStream in = new ObjectInputStream(byteIn);
+    return (Map<String, String>) in.readObject();
   }
 
 }
